264b8f5fe8318b2fcbb2becc39026292fdf7a3bc
Laytongentleman <lnmt21@protonmail.com>
2026-02-01 15:39:29 +0100

    diff

--- entity.gd
+++ entity.gd
@@ -13,3 +13,76 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+var dead = false
+
+func is_dead() -> bool:
+	return dead
+
+func die() -> void:
+	if not dead:
+		dead = true
+		var sprite = $Sprite3D
+		var tween = create_tween().set_parallel(true)
+		
+		# 1. THE ANTICIPATION: Scale up and turn white (Overcharge)
+		var ant_tween = create_tween().set_parallel(true)
+		ant_tween.tween_property(sprite, "scale", sprite.scale * 1.4, 0.1)
+		ant_tween.tween_property(sprite, "modulate", Color(10, 10, 10, 1), 0.1) # Overbright
+		await ant_tween.finished
+
+		# 2. THE EXPLOSION: Trigger the random colored particles
+		var particles = $GPUParticles3D
+		# Change material color to match the monster's "blood" or soul
+		# var mat = particles.draw_pass_1.surface_get_material(0) as StandardMaterial3D
+		# mat.albedo_color = Color(randf(), randf(), randf(), 1.0)
+		var mat = particles.draw_pass_1.surface_get_material(0) as StandardMaterial3D
+		
+		# Define our two target colors
+		var color_orange = Color(1.0, 0.5, 0.0) # Bright Orange
+		var color_blood = Color(0.6, 0.0, 0.0)  # Darker, visceral Red
+		
+		# Pick a random point between them (0.0 to 1.0)
+		var weight = randf() 
+		mat.albedo_color = color_orange.lerp(color_blood, weight)
+		mat.albedo_color.a = 0.5
+		
+		# If you want it to look extra "meaty", boost the red bias:
+		# mat.albedo_color = color_orange.lerp(color_blood, randf_range(0.4, 1.0))
+
+		particles.emitting = true
+		# 3. THE SNAP: Vanish the sprite instantly
+		sprite.visible = false
+		
+		# 4. THE SHAKE: Shake the camera (if you have one) or the parent
+		# For a game jam, we'll shake the Sprite's parent node slightly
+		var shake_tween = create_tween()
+		for i in range(5):
+			var intensity = 0.2
+			shake_tween.tween_property(sprite, "position", get_parent().position + Vector3(randf()-0.5, randf()-0.5, 0) * intensity, 0.02)
+		
+		# 5. SOUND: If you have a sound list
+		# $AudioStreamPlayer.play() 
+
+		await get_tree().create_timer(1.0).timeout # Let particles settle
+		queue_free()
+
+func bye() -> void:
+	if not dead:
+		dead = true
+		var tween = create_tween().set_parallel(true)
+		
+		# Move the Sprite3D on the X axis over 2 seconds
+		# We use global_position or position depending on your setup
+		tween.tween_property($Sprite3D, "position:x", $Sprite3D.position.x + 10.0, 0.5)\
+			.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+		
+		# 3. Fade out
+		tween.tween_property($Sprite3D, "modulate:a", 0.0, 0.12)
+		
+		# 4. Add a "Bye-bye" dandinage (Waddle)
+		# This creates a quick back-and-forth tilt during the exit
+		
+		# 5. Wait for the exit to finish
+		await tween.finished
+		queue_free()
--- main.gd
+++ main.gd
@@ -4,12 +4,20 @@ extends Node3D
 @export var MonsterSprites:Array[Texture]
 @onready var entityScene = preload("res://entity.tscn")
 @onready var timer:Timer = %Timer
+
+
+@export var gunSounds:Array[AudioStream]
+@export var monsterSounds:Array[AudioStream]
+@export var kidSounds:Array[AudioStream]
+@export var candySounds:Array[AudioStream]
+
 var candyMonster:bool = false
 var score:int = 0
 
 var rng = RandomNumberGenerator.new()
 
 enum {CHILD, MONSTER}
+var speed = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -39,25 +47,122 @@ func defeat_child(killedEntity:PathFollow3D) -> void:
 	%DefeatChild.visible = true
 	%Restart.visible = true
 
+
+func update_difficulty(delta:float) -> void:
+	# enemy spawn rate increase with score
+	if score >= 800 :
+		timer.wait_time *= 0.99
+	elif score >= 600 :
+		timer.wait_time = 0.5
+	elif score >= 500 :
+		timer.wait_time = 0.6
+	elif score >= 200:
+		timer.wait_time = 0.8
+	elif score >= 100 :
+		timer.wait_time = 1
+	elif score >= 50 :
+		timer.wait_time = 1.2
+	elif score >= 30 :
+		timer.wait_time = 1.5
+	elif score >= 10 :
+		timer.wait_time = 1.8
+	else :	
+		pass
+
+	# enemy speed
+	if score >= 3000 :
+		speed *= 1.01
+	elif score >= 2000:
+		speed = 0.7
+	elif score >= 1000 :
+		speed = 0.6
+	elif score >= 800 :
+		speed = 0.5
+	elif score >= 600 :
+		speed = 0.45
+	elif score >= 500 :
+		speed = 0.4
+	elif score >= 400 :
+		speed = 0.35
+	elif score >= 300 :
+		speed = 0.3
+	elif score >= 100 :
+		speed = 0.2
+	else :	
+		speed = 0.2
+	
+	#faster music
+	if score >= 1000 :
+		$Music.pitch_scale = 2.0
+	elif score >= 800 :
+		$Music.pitch_scale = 1.7
+	elif score >= 600 :
+		$Music.pitch_scale = 1.5
+	elif score >= 500 :
+		$Music.pitch_scale = 1.3
+	elif score >= 300 :
+		$Music.pitch_scale = 1.1
+	elif score >= 200 :
+		$Music.pitch_scale = 1.0
+	elif score >= 100 :
+		$Music.pitch_scale = 0.9
+	else :
+		pass
+
+
+	# on emit rain si seulement on est entre 300 et 500 ou 700 et 900 + ou moins 1000
+	var scoreMod1000 = score % 1000
+	if (scoreMod1000 >= 300 and scoreMod1000 < 500) or (scoreMod1000 >= 700 and scoreMod1000 < 900) :
+		$rain.emitting = true
+	else :
+		$rain.emitting = false
+	
+	# tous les 200 points on perd en luminosité
+	
+
+	
+
+
+
+
+	
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	update_difficulty(delta)
 	for entity : PathFollow3D in %Path3D.get_children() :
-		entity.progress_ratio += delta * 0.3
+		if not entity.is_dead() :
+			entity.progress_ratio += delta * speed
 		if entity.progress_ratio == 1 :
 			set_process(false)
 			%Restart.visible = true
 			if candyMonster == true :
 				%DefeatCandyMonster.visible = true
+				$monsterAudio.stream = monsterSounds.pick_random()
+				$monsterAudio.play()
 			elif entity.entity_class == CHILD :
 				%DefeatChildTimeout.visible = true
+				$kidsAudio.stream = kidSounds.pick_random()
+				$kidsAudio.play()
 			elif entity.entity_class == MONSTER :
 				%DefeatMonsterTimeout.visible = true
-			
+				$monsterAudio.stream = monsterSounds.pick_random()
+				$monsterAudio.play()
 			%AnimationPlayer.play("die")
 			return			
 			
 	if Input.is_action_just_pressed("kill") && !timer.is_stopped() :
+		var mesh = %GPUParticles3D.draw_pass_1
+	
+		var mat = mesh.surface_get_material(0) as StandardMaterial3D
+	
+		if mat:
+			mat.albedo_color = Color(randf(), randf(), randf(), 1.0)
+
 		%GPUParticles3D.emitting = true
+		$gunAudio.stream = gunSounds.pick_random()
+		$gunAudio.play()
 		if %Path3D.get_child_count() > 0:
 			var entity:PathFollow3D = %Path3D.get_child(0)
 			if (entity.entity_class == CHILD) :
@@ -65,21 +170,35 @@ func _process(delta: float) -> void:
 				set_process(false)
 				timer.stop()
 				return
-			entity.free()
-			score += 1
-			timer.wait_time *= 0.99
+			%Path3D.remove_child(entity)
+			$cimetiere.add_child(entity)
+			entity.die()
+			# entity.free()
+			score += 10
 			
 	if Input.is_action_just_pressed("candy") && !timer.is_stopped() :
+		var mesh = %candyParticule.draw_pass_1
+	
+		var mat = mesh.surface_get_material(0) as StandardMaterial3D
+	
+		if mat:
+			mat.albedo_color = Color(randf(), randf(), randf(), 1.0)
+
 		%candyParticule.restart()
+		$candyAudio.stream = candySounds.pick_random()
+		$candyAudio.play()
 		if %Path3D.get_child_count() > 0:
 			var entity:PathFollow3D = %Path3D.get_child(0)
 			if (entity.entity_class == MONSTER) :
 				timer.stop()
 				candyMonster = true
 				return
-			entity.free()
-			score += 1
-			timer.wait_time *= 0.99
+			# entity.die()
+			%Path3D.remove_child(entity)
+			$cimetiere.add_child(entity)
+			entity.bye()
+			# entity.free()
+			score += 10
 	%Score.text = str(score)
 
 
